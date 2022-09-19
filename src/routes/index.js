99f144aef5bd9a37d1dd8cf45d190b51f47f5b2e
Naim Lasker <naimlasker@gmail.com>
2022-09-20 01:27:51 +0600

    sh page added for github

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Router, Switch } from "react-router-dom"
+import { BrowserRouter, Router, Switch } from "react-router-dom"
 
 import browserHistory from "./History"
 
@@ -15,31 +15,33 @@ import About from "../pages/Public/about"
 const Routes = () => {
 
     return (
-        <Router history={browserHistory}>
-            <Switch>
-                <RouteWithLayout
-                    component={Home}
-                    exact
-                    layout={PublicMain}
-                    path='/'
-                    title='Home'
-                />
-                <RouteWithLayout
-                    component={Contact}
-                    exact
-                    layout={PublicMain2}
-                    path='/contact'
-                    title='Contact Us'
-                />
-                <RouteWithLayout
-                    component={About}
-                    exact
-                    layout={PublicMain2}
-                    path='/about'
-                    title='About Us'
-                />
-            </Switch>
-        </Router>
+        <BrowserRouter basename={window.location.pathname || ''}>
+            <Router history={browserHistory}>
+                <Switch>
+                    <RouteWithLayout
+                        component={Home}
+                        exact
+                        layout={PublicMain}
+                        path='/'
+                        title='Home'
+                    />
+                    <RouteWithLayout
+                        component={Contact}
+                        exact
+                        layout={PublicMain2}
+                        path='/contact'
+                        title='Contact Us'
+                    />
+                    <RouteWithLayout
+                        component={About}
+                        exact
+                        layout={PublicMain2}
+                        path='/about'
+                        title='About Us'
+                    />
+                </Switch>
+            </Router>
+        </BrowserRouter>
     )
 }
 
