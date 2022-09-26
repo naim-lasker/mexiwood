5c25b28ed30b39f267fc9fd6c91bc080eae50cf6
Naim Lasker <naimlasker@gmail.com>
2022-09-27 03:22:13 +0600

    furniture item rearranged

--- src/enums/home.js
+++ src/enums/home.js
@@ -1,8 +1,57 @@
 export const furnitureItems = [
     {
-        catagory: 'Soho 36',
-        img: require('../images/svg/ven/ven7.svg'),
-        imgType: 'soho_36',
+        catagory: 'Langmore 60',
+        img: require('../images/svg/ven/ven1.svg'),
+        imgType: 'langmore_60',
+        detailsList:
+            [
+                { value: 'The finish in our hardware is Black' },
+                { value: 'Our counters are ‘cultured marble with gray veining”  - 18mm thick.' },
+                { value: 'I don’t know what a vitreous sink is and we shouldn’t mention china. We should put “rectangular white porcelain under mount sink”' },
+                { value: 'Faucet and furnishing not included.' },
+                { value: 'Premium hardwood construction utilizing predominantly birch lumber and birch plywood' },
+                { value: 'Soft close door and drawer hardware' },
+                { value: 'Removable drawer dividers' },
+                { value: 'Sliding power shelf with 120v and 12v USB outlets' },
+                { value: 'The finish text should change with the associated finished vanity pic.' }
+            ],
+        tabData:
+            [
+                {
+                    title: 'Details',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
+                },
+                {
+                    title: 'Dimensions',
+                    description: [
+                        { li: `60* Vanity with Top: 60"W × 24"D × 34'H` },
+                        { li: `Backsplash: 59½"W × 1¼"D × 3'H Vanity Base: 59½W x 24"D × 322"H` },
+                        { li: `Drawer Interior: 11¾"W × 17¼"D × 3¼"H` },
+                        { li: `Cabinet Interior: 20"W × 22'D × 20¾"H` },
+                        { li: `Countertop Weight: 160 lbs.` },
+                        { li: `Vanity Base Weight: 228 Ibs.` },
+                        { li: `(without basins) 72* Vanity with Top: 72"W x 24"D × 34"H` },
+                        { li: `Backsplash: 71¾"W x 1¼"D × 3"H Vanity Base: 71¾"W × 24"D × 32¾*H` },
+                        { li: `Drawer Interior: 15"W × 17¼"D × 3¼"H` },
+                        { li: `Cabinet Interior: 24½W x 22'D × 20¾"H` },
+                        { li: `Countertop Weight: 190 lbs.` },
+                        { li: `Vanity Base Weight: 295 lbs. (without basins` },
+                    ]
+                },
+                {
+                    title: 'Specifications',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
+                },
+                {
+                    title: 'Shipping and Returns',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
+                },
+            ]
+    },
+    {
+        catagory: 'Malibu 60',
+        img: require('../images/svg/ven/ven4.svg'),
+        imgType: 'malibu_60',
         detailsList:
             [
                 { value: 'The finish in our hardware is Black' },
@@ -98,9 +147,9 @@ export const furnitureItems = [
             ]
     },
     {
-        catagory: 'Langmore 36',
-        img: require('../images/svg/ven/ven2.svg'),
-        imgType: 'langmore_36',
+        catagory: 'Prestige 60',
+        img: require('../images/svg/ven/ven6.svg'),
+        imgType: 'prestige_60',
         detailsList:
             [
                 { value: 'The finish in our hardware is Black' },
@@ -147,9 +196,9 @@ export const furnitureItems = [
             ]
     },
     {
-        catagory: 'Langmore 60',
-        img: require('../images/svg/ven/ven1.svg'),
-        imgType: 'langmore_60',
+        catagory: 'Langmore 36',
+        img: require('../images/svg/ven/ven2.svg'),
+        imgType: 'langmore_36',
         detailsList:
             [
                 { value: 'The finish in our hardware is Black' },
@@ -244,11 +293,10 @@ export const furnitureItems = [
                 },
             ]
     },
-
     {
-        catagory: 'Malibu 60',
-        img: require('../images/svg/ven/ven4.svg'),
-        imgType: 'malibu_60',
+        catagory: 'Soho 36',
+        img: require('../images/svg/ven/ven7.svg'),
+        imgType: 'soho_36',
         detailsList:
             [
                 { value: 'The finish in our hardware is Black' },
@@ -298,55 +346,6 @@ export const furnitureItems = [
         catagory: 'Prestige 36',
         img: require('../images/svg/ven/ven5.svg'),
         imgType: 'prestige 36',
-        detailsList:
-            [
-                { value: 'The finish in our hardware is Black' },
-                { value: 'Our counters are ‘cultured marble with gray veining”  - 18mm thick.' },
-                { value: 'I don’t know what a vitreous sink is and we shouldn’t mention china. We should put “rectangular white porcelain under mount sink”' },
-                { value: 'Faucet and furnishing not included.' },
-                { value: 'Premium hardwood construction utilizing predominantly birch lumber and birch plywood' },
-                { value: 'Soft close door and drawer hardware' },
-                { value: 'Removable drawer dividers' },
-                { value: 'Sliding power shelf with 120v and 12v USB outlets' },
-                { value: 'The finish text should change with the associated finished vanity pic.' }
-            ],
-        tabData:
-            [
-                {
-                    title: 'Details',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
-                },
-                {
-                    title: 'Dimensions',
-                    description: [
-                        { li: `60* Vanity with Top: 60"W × 24"D × 34'H` },
-                        { li: `Backsplash: 59½"W × 1¼"D × 3'H Vanity Base: 59½W x 24"D × 322"H` },
-                        { li: `Drawer Interior: 11¾"W × 17¼"D × 3¼"H` },
-                        { li: `Cabinet Interior: 20"W × 22'D × 20¾"H` },
-                        { li: `Countertop Weight: 160 lbs.` },
-                        { li: `Vanity Base Weight: 228 Ibs.` },
-                        { li: `(without basins) 72* Vanity with Top: 72"W x 24"D × 34"H` },
-                        { li: `Backsplash: 71¾"W x 1¼"D × 3"H Vanity Base: 71¾"W × 24"D × 32¾*H` },
-                        { li: `Drawer Interior: 15"W × 17¼"D × 3¼"H` },
-                        { li: `Cabinet Interior: 24½W x 22'D × 20¾"H` },
-                        { li: `Countertop Weight: 190 lbs.` },
-                        { li: `Vanity Base Weight: 295 lbs. (without basins` },
-                    ]
-                },
-                {
-                    title: 'Specifications',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
-                },
-                {
-                    title: 'Shipping and Returns',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
-                },
-            ]
-    },
-    {
-        catagory: 'Prestige 60',
-        img: require('../images/svg/ven/ven6.svg'),
-        imgType: 'prestige_60',
         detailsList:
             [
                 { value: 'The finish in our hardware is Black' },
