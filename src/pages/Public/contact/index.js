e62657db43027bae3ba188e1b381ef35b86c1fce
tasfique-alam <tasfiquealam@gmail.com>
2022-09-26 23:03:10 +0600

    fix

--- src/pages/Public/contact/index.js
+++ src/pages/Public/contact/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GoogleMapReact from 'google-map-react';
 import ReCAPTCHA from "react-google-recaptcha";
 
@@ -14,6 +14,15 @@ const Contact = () => {
         },
         zoom: 11
     };
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [msg, setMsg] = useState('')
+
+    const submit = () => {
+        console.log('hii');
+    }
 
     return (
         < div className='touch-content'>
@@ -64,21 +73,21 @@ const Contact = () => {
                         </div>
                     </div>
                     <div className="col-12 col-md-7 contact-form mt-5 mt-md-0">
-                        <form id="contact-form" action="https://formsubmit.co/tasfiquealam@gmail.com" method="POST">
+                        <form id="contact-form" method="POST">
                             <div className="form-group d-flex align-item-center justify-content-between mb-0">
-                                <input placeholder='FirstName' type="text" required className="form-control mr-3" />
-                                <input placeholder='LastName' type="text" required className="form-control" />
+                                <input placeholder='FirstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" required className="form-control mr-3" />
+                                <input placeholder='LastName' value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" required className="form-control" />
                             </div>
                             <div className="form-group">
-                                <input type="email" placeholder='Email address' required className="form-control" aria-describedby="emailHelp" />
+                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email address' required className="form-control" aria-describedby="emailHelp" />
                             </div>
                             <div className="form-group">
-                                <textarea className="form-control" required placeholder='Message' rows="5" />
+                                <textarea className="form-control" value={msg} onChange={(e) => setMsg(e.target.value)} required placeholder='Message' rows="5" />
                             </div>
                             <ReCAPTCHA
                                 sitekey="Your client site key"
                             />
-                            <button type="submit" className="btn btn-send">SEND</button>
+                            <button type="submit" onClick={submit} className="btn btn-send">SEND</button>
                         </form>
 
                     </div>
